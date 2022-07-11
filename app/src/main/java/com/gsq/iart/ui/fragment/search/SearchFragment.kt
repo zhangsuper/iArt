714667c00b25df792f2af27134668ba9479deebd
super <773609213@qq.com>
2022-07-12 00:44:46 +0800

    搜索功能

--- app/src/main/java/com/gsq/iart/ui/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/gsq/iart/ui/fragment/search/SearchFragment.kt
@@ -1,12 +1,65 @@
 package com.gsq.iart.ui.fragment.search
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.gsq.iart.app.base.BaseFragment
+import com.gsq.iart.app.ext.init
+import com.gsq.iart.app.util.CacheUtil
 import com.gsq.iart.databinding.FragmentSearchBinding
+import com.gsq.iart.ui.adapter.SearchHistoryAdapter
+import com.gsq.iart.ui.adapter.SearchHotAdapter
+import com.gsq.iart.viewmodel.SearchViewModel
 import com.gsq.mvvm.base.viewmodel.BaseViewModel
+import com.gsq.mvvm.ext.nav
+import com.gsq.mvvm.ext.util.toJson
+import kotlinx.android.synthetic.main.fragment_search.*
 
-class SearchFragment: BaseFragment<BaseViewModel, FragmentSearchBinding>() {
+/**
+ * 搜索页
+ */
+class SearchFragment: BaseFragment<SearchViewModel, FragmentSearchBinding>() {
+
+    private val historyAdapter: SearchHistoryAdapter by lazy { SearchHistoryAdapter(arrayListOf()) }
+
+    private val hotAdapter: SearchHotAdapter by lazy { SearchHotAdapter(arrayListOf()) }
+
     override fun initView(savedInstanceState: Bundle?) {
+        back_btn.setOnClickListener {
+            nav().navigateUp()
+        }
+        //创建流式布局layout
+        val layoutManager = FlexboxLayoutManager(context)
+        //方向 主轴为水平方向，起点在左端
+        layoutManager.flexDirection = FlexDirection.ROW
+        //左对齐
+        layoutManager.justifyContent = JustifyContent.FLEX_START
+        //初始化搜搜历史Recyclerview
+        search_historyRv.init(layoutManager, historyAdapter, false)
+        search_hotRv.init(layoutManager, hotAdapter, false)
+    }
+
+    override fun lazyLoadData() {
+        //获取历史搜索词数据
+        mViewModel.getHistoryData()
+        //获取热门数据
+        mViewModel.getHotData()
+    }
+
+    override fun createObserver() {
+        super.createObserver()
+        mViewModel.searchHistoryList.observe(viewLifecycleOwner, Observer {
+            historyAdapter.data = it
+            historyAdapter.notifyDataSetChanged()
+            CacheUtil.setSearchHistoryData(it.toJson())
+        })
+
+        mViewModel.searchHotList.observe(viewLifecycleOwner, Observer {
+
+        })
     }
 
 }
---
+++ app/src/main/java/com/gsq/iart/viewmodel/SearchViewModel.kt
@@ -0,0 +1,37 @@
+package com.gsq.iart.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.gsq.iart.app.network.apiService
+import com.gsq.iart.app.network.stateCallback.ListDataUiState
+import com.gsq.iart.app.util.CacheUtil
+import com.gsq.iart.data.bean.WorksBean
+import com.gsq.mvvm.base.viewmodel.BaseViewModel
+import com.gsq.mvvm.ext.launch
+import com.gsq.mvvm.ext.request
+
+class SearchViewModel: BaseViewModel() {
+
+    var searchHistoryList: MutableLiveData<ArrayList<String>> = MutableLiveData()
+    var searchHotList: MutableLiveData<ArrayList<String>> = MutableLiveData()
+
+    /**
+     * 获取热门数据
+     */
+    fun getHotData() {
+//        request({ apiService.getSearchData() }, hotData)
+    }
+
+    /**
+     * 获取历史数据
+     */
+    fun getHistoryData() {
+        launch({
+            CacheUtil.getSearchHistoryData()
+        }, {
+            searchHistoryList.value = it
+        }, {
+            //获取本地历史数据出异常了
+        })
+    }
+
+}
