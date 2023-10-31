b81d14135032984ede492343ae4ac0df8c87e330
zhishui <773609213@qq.com>
2023-10-31 16:31:30 +0800

    图典二级标签付费逻辑

--- app/src/main/java/com/gsq/iart/ui/adapter/DictionaryMenuAdapter.kt
+++ app/src/main/java/com/gsq/iart/ui/adapter/DictionaryMenuAdapter.kt
@@ -2,6 +2,7 @@ package com.gsq.iart.ui.adapter
 
 import android.graphics.Typeface
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -9,12 +10,16 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.blankj.utilcode.util.SizeUtils
+import com.blankj.utilcode.util.ToastUtils
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.gsq.iart.R
+import com.gsq.iart.app.util.CacheUtil
 import com.gsq.iart.app.weight.recyclerview.GridItemDecoration
 import com.gsq.iart.data.bean.DictionaryMenuBean
+import com.gsq.mvvm.ext.view.gone
 import com.gsq.mvvm.ext.view.onClick
+import com.gsq.mvvm.ext.view.visible
 
 class DictionaryMenuAdapter : BaseQuickAdapter<DictionaryMenuBean, BaseViewHolder>(R.layout.item_dictionary_layout) {
 
@@ -29,6 +34,7 @@ class DictionaryMenuAdapter : BaseQuickAdapter<DictionaryMenuBean, BaseViewHolde
         var mMenuLogo = holder.getView<ImageView>(R.id.menu_logo)
         var mMenuName = holder.getView<TextView>(R.id.menu_name)
         var mRecyclerView = holder.getView<RecyclerView>(R.id.recycler_view)
+        var frameUpLayout = holder.getView<LinearLayout>(R.id.frame_up_layout)
         mRecyclerView.layoutManager  = StaggeredGridLayoutManager(
             3, StaggeredGridLayoutManager.VERTICAL
         )
@@ -40,13 +46,32 @@ class DictionaryMenuAdapter : BaseQuickAdapter<DictionaryMenuBean, BaseViewHolde
             )
         )
         var mSecondMenuAdapter = DictionarySecondMenuAdapter()
+//        mSecondMenuAdapter.min_count = 2
         mRecyclerView.adapter = mSecondMenuAdapter
         mMenuName.text = item.name
         if(item.subs!= null){
-            mSecondMenuAdapter.data = item.subs
+            mSecondMenuAdapter.setData(item.subs)
         }
         mSecondMenuAdapter.setOnItemClickListener { adapter, view, position ->
             mClickListener?.invoke(item, position+1)
+        }
+        mSecondMenuAdapter?.setOnItemChildClickListener { adapter, view, position ->
+            if (view.id == R.id.frame_down_layout) {
+                //展开 需要超级会员
+                if(CacheUtil.getUser()?.memberType != 1){
+                    ToastUtils.showLong("需要超级会员才能展开")
+                }else {
+                    mSecondMenuAdapter.setExpendStatus(true)
+                    mSecondMenuAdapter.setData(item.subs)
+                    frameUpLayout.visible()
+                }
+            }
+        }
+        frameUpLayout.onClick {
+            //收起
+            frameUpLayout.gone()
+            mSecondMenuAdapter.setExpendStatus(false)
+            mSecondMenuAdapter.setData(item.subs)
         }
         leftView.onClick {
             mClickListener?.invoke(item,0)
--- app/src/main/java/com/gsq/iart/ui/adapter/DictionarySecondMenuAdapter.kt
+++ app/src/main/java/com/gsq/iart/ui/adapter/DictionarySecondMenuAdapter.kt
@@ -1,16 +1,58 @@
 package com.gsq.iart.ui.adapter
 
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.gsq.iart.R
 import com.gsq.iart.data.bean.DictionaryMenuBean
+import com.gsq.mvvm.ext.view.gone
+import com.gsq.mvvm.ext.view.visible
 
 class DictionarySecondMenuAdapter : BaseQuickAdapter<DictionaryMenuBean, BaseViewHolder>(R.layout.item_dictionary_second_layout) {
 
+    var isExpand = false
+
+    var min_count = 5
+
+    init {
+        addChildClickViewIds(R.id.frame_down_layout)
+    }
+
+    fun setData(list: MutableList<DictionaryMenuBean>){
+        if(list.size> min_count+1 && !isExpand){
+            this.data = list.subList(0,min_count+1)
+        }else {
+            this.data = list
+        }
+        notifyDataSetChanged()
+    }
+
+    fun setExpendStatus(isExpand: Boolean){
+        this.isExpand = isExpand
+
+//        notifyDataSetChanged()
+    }
+
     override fun convert(holder: BaseViewHolder, item: DictionaryMenuBean) {
+        var frameDownLayout = holder.getView<LinearLayout>(R.id.frame_down_layout)
         var mMenuName = holder.getView<TextView>(R.id.menu_name)
         mMenuName.text = item.name
+
+        if(isExpand){
+            //展开
+            frameDownLayout.gone()
+            mMenuName.visible()
+        }else{
+            //收起
+            if(holder.layoutPosition == min_count){
+                frameDownLayout.visible()
+                mMenuName.gone()
+            }else{
+                frameDownLayout.gone()
+                mMenuName.visible()
+            }
+        }
     }
 
 }
